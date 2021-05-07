81afc6d3a2a2f9fbaf21a8d6c7227dd12def8327
TheSola10 <me@thesola.io>
2021-05-07 13:36:36 +0200

    Added comments to Haskell implementation

--- star_seller.hs
+++ star_seller.hs
@@ -10,22 +10,30 @@ import Text.Read
 putStars :: Int -> [Char]
 putStars n = take n (repeat '*')    -- Demoing a lazy iterator
 
-
+-- One interesting point in Haskell is that, being a purely functional
+-- language, there is no real concept of "loops". Those have to be implemented
+-- as recursive functions.
 askStars :: IO Int
 askStars =  do
             putStr "How many stars do you want? "
             ask <- getLine
             let value = readMaybe ask :: Maybe Int
+            -- The Maybe wrapping type forces you to use pattern-matching to
+            -- consider the case where there is no value (due to an error for
+            -- instance).
             case value of
               Just n -> if n <=> (0,200)
                            then return n
                            else askStars
               Nothing -> askStars
 
-
 (<=>) :: Ord a => a -> (a,a) -> Bool
 (<=>) x (min, max) = x >= min && x <= max
 
+
+-- Interestingly, the IO wrapping type (which signifies impurity) cannot be
+-- removed. This way, you have to be mindful of how much code you have to pull
+-- into that context. It improves reusability drastically.
 main :: IO ()
 main =  do
         putStr "What is your name? "
