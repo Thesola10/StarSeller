8b1c05744255dfdfa332eb856c30a3b713e98d08
TheSola10 <me@thesola.io>
2021-11-24 11:18:04 +0100

    python following best practices

--- star_seller.py
+++ star_seller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # The Star Seller exercise in algorithmics,
-# written in Python 3.                      [17 lines of code]
+# written in Python 3.                      [18 lines of code]
 
 '''
 Not much to say. Python is concise and straightforward, and its exigeance in
@@ -13,23 +13,24 @@ speed up re-execution, it is important to keep that in mind.
 '''
 
 def star_seller():
-    stars = 0
-    name = str()
-    valid = False
+    name : str
+    stars : int     = 0
+    valid : bool    = False
 
     while not valid:
         name = input("What is your name? ")
         if len(name) > 0:
             valid = True
     valid = False
-    print("Hello, %s!" %name)
+    print(f"Hello, {name}!")
     while not valid:
         stars = int(input("How many stars do you want? "))
         if (stars < 200):
             print("*" * stars)
             valid = True
-    print("Goodbye, %s" %name)
+    print(f"Goodbye, {name}")
 
-star_seller()
+if __name__ == '__main__':
+    star_seller()
 
 # This language is case- and indentation-sensitive
