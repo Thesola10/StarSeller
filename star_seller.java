660334f285d738510b6b22ed736e339e5b5bea7a
TheSola10 <me@thesola.io>
2022-01-10 09:07:22 +0100

    Finished Java implementation

--- star_seller.java
+++ star_seller.java
@@ -1,17 +1,39 @@
 /**
  * The Star Seller exercise in algorithmics,
- * written in Java.                         [Unfinished]
+ * written in Java.                         [27 lines of code]
  *
  * Java is often cited as the ultimate object-oriented language. I guess I can
  * agree, but it is also often heavy in resources due to requiring a virtual
  * machine. Still, the object concept is very expressive (albeit verbose).
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class star_seller
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Hello world");
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        String name;
+        int stars = -1;
+
+        do {
+            System.out.print("What is your name? ");
+            name = r.readLine();
+        } while (name.isBlank());
+        System.out.println("Hello, " + name + "!");
+        do {
+            System.out.print("How many stars do you want? ");
+            try {
+                stars = Integer.parseInt(r.readLine());
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        } while (stars < 0 || stars > 200);
+        System.out.println("*".repeat(stars));
+        System.out.println("Goodbye, " + name);
     }
 }
 
